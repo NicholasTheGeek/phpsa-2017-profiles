612734f98663228fb8195c2631237ba69aaacbea
Ezekiel Motlana <ezekielm@miway.co.za>
2017-10-05 14:23:48 +0200

    add Nicholas profile

---
+++ src/Profiles/NicholasProfile.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: ezekiel
+ * Date: 2017/10/05
+ * Time: 2:16 PM
+ */
+
+namespace mostertb\PHPSA2017Profiles\Profiles;
+
+
+class NicholasProfile extends mostertb\PHPSA2017Profiles\Abstracts\AbstractProfile
+{
+    public function getName()
+    {
+        return "Nicholas Masuku";
+    }
+
+    public function  getBiography()
+    {
+        return "Nicholas the geek";
+    }
+}
